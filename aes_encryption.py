131f8e669a126772586cfddd5f0bbeaeda3fc859
Fabio Cordeiro <fabiojpcordeiro@outlook.com>
2026-02-19 12:50:45 -0300

    change from matrix to bytearray

--- aes_encryption.py
+++ aes_encryption.py
@@ -1,244 +1,208 @@
-from aes_base_tables import base_sbox, base_rcon
+import aes_base_tables
 
 class AES_Encryption:
-    def __init__(self, key_bytes):
+    def __init__(self, key):
 
         # Initialize base vectors
-        self.s_box = base_sbox.copy()
-        self.rcon = base_rcon.copy()
+        self.s_box = aes_base_tables.base_sbox.copy()
+        self.rcon = aes_base_tables.base_rcon.copy()
+        self.galois2 = aes_base_tables.galois2.copy()
+        self.galois3 = aes_base_tables.galois3.copy()
+        self.galois9 = aes_base_tables.galois9.copy()
+        self.galois11 = aes_base_tables.galois11.copy()
+        self.galois13 = aes_base_tables.galois13.copy()
+        self.galois14 = aes_base_tables.galois14.copy()
 
         # Key expansion
-        key = self._convert_to_4x4_matrix(key_bytes)
-        self.all_words = self._key_expansion(key)
+        self.round_keys = self._key_expansion(bytearray(key))
 
         # Generate inverse s-box
-        self.inv_box = [0] * 256
+        self.inv_box = bytearray(256)
         for i in range(256):
             sbox_value = self.s_box[i]
             self.inv_box[sbox_value] = i
 
     
     #Helpers
+    def _rot_word(self, word):
+        return word[1:] + word[:1]
+    
+    def _sub_word(self, word):
+        new_word = []
+        for b in word:
+            new_word.append(self.s_box[b])
+        return bytearray(new_word)
+
+    def _xor_words(self, w1, w2):
+        return bytearray(a ^ b for a, b in zip(w1, w2))
+
     def _xtime(self, b):
         if b & 0x80:
             return ((b << 1) ^ 0x1b) & 0xFF
         return (b << 1) & 0xFF
     
-    def _mul_2(self, x):
-        return self._xtime(x)
-
-    def _mul_3(self, x):
-        return self._xtime(x) ^ x
-
-    def _mul_9(self, x):
-        return self._xtime(self._xtime(self._xtime(x))) ^ x
-
-    def _mul_11(self, x):
-        return self._xtime(self._xtime(self._xtime(x))) ^ self._xtime(x) ^ x
-
-    def _mul_13(self, x):
-        return self._xtime(self._xtime(self._xtime(x))) ^ self._xtime(self._xtime(x)) ^ x
-
-    def _mul_14(self, x):
-        return self._xtime(self._xtime(self._xtime(x))) ^ self._xtime(self._xtime(x)) ^ self._xtime(x)
-
+    def _pad(self, data):
+        pad_len = 16 - (len(data) % 16)
+        return data + bytes([pad_len] * pad_len)
+    
+    def _unpad(self, data):
+        pad_len = data[-1]
+        return data[:-pad_len]
     
     # Bytes substitution and inverse version
     def _sub_bytes(self, state, s_box):
-        for i in range(0, len(state)):
-            for j in range(0, len(state[i])):
-                state[i][j] = s_box[state[i][j]]
+        for i in range(16):
+            state[i] = s_box[state[i]]
         return state
     
     def _inv_sub_bytes(self, state, inv_s_box):
-        for i in range(0, len(state)):
-            for j in range(0, len(state[i])):
-                state[i][j] = inv_s_box[state[i][j]]
+        for i in range(16):
+            state[i] = inv_s_box[state[i]]
         return state
 
     # Shiftrows and inverse version
-    def _shiftrows(self, arr):
-        counter = 1
-        for i in range(1, 4):
-            row = arr[i]
-            arr[i] = row[counter:] + row[:counter]
-            counter += 1
-        return arr
+    def _shiftrows(self, s):
+        #row 1
+        s[1] , s[5], s[9], s[13] = s[5], s[9], s[13], s[1]
+        #row 2
+        s[2], s[6], s[10], s[14] = s[10], s[14], s[2], s[6]
+        #row 3
+        s[3], s[7], s[11], s[15] = s[15], s[3], s[7], s[11]
+        return s
 
-    def _inv_shiftrows(self, arr):
-        counter = 1
-        for i in range(1, 4):
-            row = arr[i]
-            arr[i] = row[-counter:] + row[:-counter]
-            counter += 1
-        return arr
+    def _inv_shiftrows(self, s):
+        #row 1
+        s[1], s[5], s[9], s[13] = s[13], s[1], s[5], s[9]
+        #row 2
+        s[2], s[6], s[10], s[14] = s[10], s[14], s[2], s[6]
+        #row 3
+        s[3], s[7], s[11], s[15] = s[7], s[11], s[15], s[3]
+        return s
 
     # MixColumns step and inverse
-    def _mix_columns(self, column):
-        nb0 = self._xtime(column[0]) ^ (self._xtime(column[1]) ^ column[1]) ^ column[2] ^ column[3]
-        nb1 = column[0] ^ self._xtime(column[1]) ^ (self._xtime(column[2]) ^ column[2]) ^ column[3]
-        nb2 = column[0] ^ column[1] ^ self._xtime(column[2]) ^ (self._xtime(column[3]) ^ column[3])
-        nb3 = (self._xtime(column[0]) ^ column[0]) ^ column[1] ^ column[2] ^ self._xtime(column[3])
+    def _mix_columns(self, state):
+        for i in range(4):
+            base = i * 4
 
-        return [nb0, nb1, nb2, nb3]
-    
-    def _inv_mix_columns(self, col):
-        n0 = self._mul_14(col[0]) ^ self._mul_11(col[1]) ^ self._mul_13(col[2]) ^ self._mul_9(col[3])
-        n1 = self._mul_9(col[0]) ^ self._mul_14(col[1]) ^ self._mul_11(col[2]) ^ self._mul_13(col[3])
-        n2 = self._mul_13(col[0]) ^ self._mul_9(col[1]) ^ self._mul_14(col[2]) ^ self._mul_11(col[3])
-        n3 = self._mul_11(col[0]) ^ self._mul_13(col[1]) ^ self._mul_9(col[2]) ^ self._mul_14(col[3])
-        return [n0, n1, n2, n3]
+            a0 = state[base]
+            a1 = state[base + 1]
+            a2 = state[base + 2]
+            a3 = state[base + 3]
 
-
-    def _start_mix(self, state, mode="normal"):
-        for i in range(0, 4):
-            column = []
-            for j in range(0,4):
-                column.append(state[j][i])
-
-            if mode == "normal":
-                new_column = self._mix_columns(column)
-            if mode == "inversed":
-                new_column = self._inv_mix_columns(column)
-
-            for j in range(0,4):
-                state[j][i] = new_column[j]
-
+            state[base] = self._xtime(a0) ^ (self._xtime(a1) ^ a1) ^ a2 ^ a3
+            state[base + 1] = a0 ^ self._xtime(a1) ^ (self._xtime(a2) ^ a2) ^ a3
+            state[base + 2] = a0 ^ a1 ^ self._xtime(a2) ^ (self._xtime(a3) ^ a3)
+            state[base + 3] = (self._xtime(a0) ^ a0) ^ a1 ^ a2 ^ self._xtime(a3)
         return state
-            
+
+    def _inv_mix_columns(self, state):
+        for i in range(4):
+            base = i * 4
+            a0 = state[base]
+            a1 = state[base + 1]
+            a2 = state[base + 2]
+            a3 = state[base + 3]
+
+            state[base]     = self.galois14[a0] ^ self.galois11[a1] ^ self.galois13[a2] ^ self.galois9[a3]
+            state[base + 1] = self.galois9[a0]  ^ self.galois14[a1] ^ self.galois11[a2] ^ self.galois13[a3]
+            state[base + 2] = self.galois13[a0] ^ self.galois9[a1]  ^ self.galois14[a2] ^ self.galois11[a3]
+            state[base + 3] = self.galois11[a0] ^ self.galois13[a1] ^ self.galois9[a2]  ^ self.galois14[a3]
+        return state
+
     def _add_round_key(self, state, key):
-        for i in range(0,4):
-            for j in range(0,4):
-                state[i][j] = state[i][j] ^ key[i][j]
+        for i in range(16):
+            state[i] ^= key[i]
         return state
-
-    # Conversions
-    def _convert_to_4x4_matrix(self, arr):
-        new_arr = [[], [], [], []]
-        row = 0
-        for c in arr:
-            new_arr[row].append(c)
-            row += 1
-            if row > 3: row = 0
-        return new_arr
-
-    def _convert_words_to_matrix(self, words):
-        matrix = [[],[],[],[]]
-        if len(words) != 4:
-            return "An error has ocurred."
-        for i in range(0,4):
-            for j in range(0,4):
-                matrix[j].append(words[i][j])
-        return matrix
 
      #Key expansion
+    
     def _key_expansion(self, key):
         words= []
-        r_con_counter = 0
-        for i in range(0,4):
-            word = []
-            for j in range(0,4):
-                word.append(key[j][i])
-            words.append(word)
 
+        for i in range(4):
+            word = key[i*4 : (i+1) *4 ]
+            words.append(bytearray(word))
+            
         for i in range(4,44):
+            temp = words[i-1].copy()
 
             if i % 4 == 0:
-                last_word = words[i-1].copy()
-                new_word = last_word[1:] + last_word[:1]
+                temp = self._rot_word(temp)
+                temp = self._sub_word(temp)
+                temp[0] ^= self.rcon[(i // 4) - 1]
 
-                for j in range(4):
-                    new_word[j] = self.s_box[new_word[j]]
+            new_word = self._xor_words(words[i-4], temp)
+            words.append(new_word)
 
-                new_word[0] ^= int(self.rcon[r_con_counter], 16)
-                r_con_counter += 1
-                
-                for j in range(4):
-                    new_word[j] ^= words[i-4][j]
-
-                words.append(new_word)
-            else:
-                last_word = words[i-1]
-                four_behind = words[i-4]
-                new_word = []
-                for j in range(4):
-                    new_word.append(last_word[j] ^ four_behind[j])
-                words.append(new_word)
-        return words
-
-    def matrix_to_bytes(self, matrix):
-        plain_text = []
-        for i in range(4):
+        round_keys = []
+        for i in range(0, 44, 4):
+            rk = bytearray()
             for j in range(4):
-                plain_text.append(matrix[j][i])
-        return bytes(plain_text)
-    
+                rk.extend(words[i + j])
+            round_keys.append(rk)
+                
+        return round_keys
+
     # ENCRYPTION CALL
     def encrypt(self, plain_text_bytes):
+        state = bytearray(plain_text_bytes)
 
-        state = self._convert_to_4x4_matrix(plain_text_bytes)
+        # Step 0
+        state = self._add_round_key(state, self.round_keys[0])
 
-        # Get fist key a XOR with base text (Step 0)
-        first_key = self._convert_words_to_matrix(self.all_words[0:4])
-        state = self._add_round_key(state, first_key)
-
-        # Run steps 1 to 10 of AES algorithm
+        # Step 1 - 10
         for r in range(1, 10):
             state = self._sub_bytes(state, self.s_box)
             state = self._shiftrows(state)
-            state = self._start_mix(state)
+            state = self._mix_columns(state)
+            state = self._add_round_key(state, self.round_keys[r])
 
-            round_key_matrix = self._convert_words_to_matrix(self.all_words[r*4: r*4+4])
-            state = self._add_round_key(state, round_key_matrix)
-
-        # Run step 11
+        # Step 11
         state = self._sub_bytes(state, self.s_box)
         state = self._shiftrows(state)
-        final_key_matrix = self._convert_words_to_matrix(self.all_words[40:44])
-        state = self._add_round_key(state, final_key_matrix)
+        state = self._add_round_key(state, self.round_keys[10])
         return state
 
     #DECRYPTION CALL
     def decrypt(self, cipher_text):
-        state = [row[:] for row in cipher_text]
+        state = bytearray(cipher_text)
 
-
-        fist_key = self._convert_words_to_matrix(self.all_words[40:44])
-        state = self._add_round_key(state, fist_key)
+        state = self._add_round_key(state, self.round_keys[10])
 
         for r in range(9, 0, -1):
-            # Inverted sub_bytes and shiftrows
             state = self._inv_shiftrows(state)
             state = self._inv_sub_bytes(state, self.inv_box)
 
-            round_key_matrix = self._convert_words_to_matrix(self.all_words[r*4: r*4+4])
-            state = self._add_round_key(state, round_key_matrix)
-
-            state = self._start_mix(state, mode="inversed") 
+            state = self._add_round_key(state, self.round_keys[r])
+            state = self._inv_mix_columns(state) 
 
         state = self._inv_shiftrows(state)
         state = self._inv_sub_bytes(state, self.inv_box)
-        final_key_matrix = self._convert_words_to_matrix(self.all_words[0:4])
-        state = self._add_round_key(state, final_key_matrix)
+
+        state = self._add_round_key(state, self.round_keys[0])
         return state
     
     def test():
         aes = AES_Encryption(b"Thats my Kung Fu")
-        cipher = aes.encrypt(b"Two One Nine Two")
+        plain_text = b"Two One Nine Two"
         expected_cipher = "29c3505f571420f6402299b31a02d73a"
-        
-        if aes.matrix_to_bytes(cipher).hex() == expected_cipher:
-            print("Encryption successful")
-        else:
-            print("Something went wrong")
 
-        plain_text = aes.decrypt(cipher)
-        if aes.matrix_to_bytes(plain_text) == b"Two One Nine Two":
-            print("Decryption successfull")
+        cipher = aes.encrypt(plain_text)
+        decipher = aes.decrypt(cipher)
+
+        if cipher.hex() == expected_cipher:
+            print("Encryption successfull")
+
+            if decipher == plain_text:
+                print("Decryption successfull")
+            else:
+                print("Decryption failed")
         else:
-            print("Decryption Failed")
-        
+            print("Encription Failed")
+
+
 AES_Encryption.test()
+
 
 
 
